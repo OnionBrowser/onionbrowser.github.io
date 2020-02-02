93e9daa52b0cf4de81585bb4d3eb80b38816e9c3
MobiTech1191 <vik_footope@outlook.com>
2020-02-02 19:34:07 +0200

    [Completed] mobile responsive pages

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,15 @@
 (function ($) {
     "use strict";
-	
-	$(document).ready(function() {
-       $('#responsive-menu-button').sidr({
-          name: 'sidr-main',
-          source: '#navigation'
+
+    $(document).ready(function () {
+        $('#responsive-menu-button').sidr({
+            name: 'sidr-main',
+            source: '.main-nav',
+            side: 'right'
         });
-	   
-	});
-	
-	
-	
+
+    });
+
+
+
 })(jQuery);
