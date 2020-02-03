2fbe128a16d27c1502bc0de016f2394d10571f57
MobiTech1191 <vik_footope@outlook.com>
2020-02-03 21:51:32 +0200

    [Completed] HTML/CSS work done

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,18 @@
     "use strict";
 
     $(document).ready(function () {
-        $('#responsive-menu-button').sidr({
+        /*$('#responsive-menu-button').sidr({
             name: 'sidr-main',
             source: '.main-nav-for-mobile',
             side: 'right'
+        });*/
+
+        $('#responsive-menu-button').on('click', function () {
+            $('#sidr').addClass('appear');
+        });
+
+        $('#close-button').on('click', function () {
+            $('#sidr').removeClass('appear');
         });
 
     });
